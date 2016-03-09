250f80d3b4653788a533da9e901adff2fd692419
dhet <aktives.faultier@gmx.de>
2016-03-09 11:00:33 +0100

    added documentation for CrawlPrefs

--- src/crawling/CrawlPrefs.scala
+++ src/crawling/CrawlPrefs.scala
@@ -2,10 +2,17 @@ package crawling
 
 import java.nio.file.Paths
 
-import com.sun.deploy.util.StringUtils
 import graph.LabelEntry
 import webgraph.{Weblink, Webpage}
 
+/**
+  * Object that contains all crawler properties. The properties should only be defined before the crawling process has
+  * started. The crawler gets the preferences during the process as needed.
+  * <br/><br/>
+  * This object contains a list of analyze functions. Each of the functions is called by the crawler once a site has
+  * been crawled. Analyze functions have the signature
+  * `([[webgraph.Webpage]]) => [[scala.Option]] [ [[graph.LabelEntry]] ]`.
+  */
 object CrawlPrefs {
   var maxDepth = 2
   var threads = 3
@@ -15,10 +22,28 @@ object CrawlPrefs {
 
   addPageAnalyzeFunction(extractPageTitle)
 
+  /**
+    * Add an analyze function to the list of analyze functions
+    * @param alg  The function to add
+    */
   def addPageAnalyzeFunction(alg : (Webpage) => Option[LabelEntry]) = {
     analyzeFunctionsPages = analyzeFunctionsPages :+ alg
   }
 
+
+
+  /**
+    *
+    * Default analyze functions
+    *
+    */
+
+  /**
+    * Extract the page title from a given website's HTML via regular expressions. The page title can be found between
+    * the {{{<title>}}} tags.
+    * @param page The page to analyze
+    * @return     An optional link label containing ''page-title'' as key and the extracted page title as value
+    */
   private def extractPageTitle(page : Webpage) : Option[LabelEntry] = {
     val selectionPattern = """<title>(.*?)</title>""".r.unanchored
     selectionPattern.findFirstMatchIn(page.content) match{
@@ -27,7 +52,4 @@ object CrawlPrefs {
     }
   }
 }
-
-
-
 
